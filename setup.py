ccd65ca26b6d9619246c89229578e163efd031db
David Hooton <david@hooton.org>
2024-07-05 14:12:51 +1000

    ✨ feat(setup.py):add dynamic version retrieval and update description
    Added a function to dynamically retrieve the version of the package from the
    'version.py' file. Updated the package description to include encryption of
    unencrypted secrets using sops and age/gpg. Updated dependencies in the
    'install_requires' section.
    
    Signed-off-by: David Hooton <david@hooton.org>

--- setup.py
+++ setup.py
@@ -2,15 +2,43 @@
 Setup configuration for the sops-pre-commit package.
 """
 
+import os
+import re
+
 from setuptools import find_packages, setup  # type: ignore
+
+
+def get_version():
+    """
+    Retrieve the version of the package from the version.py file.
+
+    Returns:
+        str: The version string.
+
+    Raises:
+        RuntimeError: If the version string cannot be found.
+    """
+    version_file = os.path.join(os.path.dirname(__file__), "version.py")
+    with open(version_file, encoding="utf-8") as version_file_obj:
+        code = version_file_obj.read()
+    version_match = re.search(
+        r"^__version__ = ['\"]([^'\"]*)['\"]", code, re.M
+    )
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
 
 setup(
     name="sops-pre-commit",
-    description="Check for unencrypted Kubernetes secrets in manifest files"
-    "and encrypt them before allowing git to push. "
-    "Forked from https://github.com/onedr0p/sops-pre-commit",
+    version=get_version(),
+    packages=find_packages(),
+    description="Check for unencrypted secrets and encrypt them using sops and"
+    "age/gpg. Forked from https://github.com/onedr0p/sops-pre-commit",
     url="https://github.com/djh00t/sops-pre-commit",
-    version="1.0.0",
     author="David Hooton",
     author_email="sops-pre-commit+david@hooton.org",
     platforms="linux",
@@ -25,7 +53,12 @@ setup(
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy",
     ],
-    packages=find_packages("."),
+    install_requires=[
+        "argparse",
+        "datetime",
+        "typing",
+        "ruamel.yaml",
+    ],
     entry_points={
         "console_scripts": [
             "forbid_secrets = hooks.forbid_secrets:main",
