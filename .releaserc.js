8e8a904e17363ae5a7f8879f346eae2fd1553eef
David Hooton <david@hooton.org>
2024-07-31 14:59:30 +1000

    🔧 chore(.releaserc.js): update release configuration and plugins
    - Changed branch configuration to specify main and prerelease for release.
    - Updated repository URL.
    - Refactored plugin settings, including removal of unused exec commands and
    addition of README.md and pyproject.toml to assets.
    - Specified preset to use conventional commits and defined release rules for
    various commit types.
    
    Signed-off-by: David Hooton <david@hooton.org>

--- .releaserc.js
+++ .releaserc.js
@@ -1,50 +1,40 @@
 module.exports = {
   branches: [
-    `${process.env.RELEASE_BRANCH || 'release'}`
+    { name: "main" },
+    { name: "release", prerelease: true }
   ],
+  repositoryUrl: "https://github.com/djh00t/sops-pre-commit.git",
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/changelog",
-    [
-      "@semantic-release/exec",
-      {
-        "prepareCmd": "python setup.py sdist bdist_wheel"
-      }
-    ],
-    [
-      "@semantic-release/git",
-      {
-        "assets": [
-          "CHANGELOG.md",
-          "setup.py",
-          "version.py"
-        ],
-        "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
-        "branch": `release-${process.env.EPOCH_TIME || 'default'}`
-      }
-    ],
-    [
-      "@semantic-release/exec",
-      {
-        "prepareCmd": "git add CHANGELOG.md setup.py version.py && git commit -m 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}' && git push origin release-${process.env.EPOCH_TIME || 'default'}"
-      }
-    ],
-    [
-      "@semantic-release/github",
-      {
-        "successComment": false,
-        "failComment": false,
-        "addReleases": "bottom",
-        "createRelease": true,
-        "assets": "dist/*"
-      }
-    ],
-    [
-      "@semantic-release/exec",
-      {
-        "publishCmd": "gh pr create --title 'chore(release): ${nextRelease.version}' --body 'This PR includes the release ${nextRelease.version}.\n\n${nextRelease.notes}' --base main --head release-${process.env.EPOCH_TIME || 'default'}"
-      }
-    ]
-  ]
+    "@semantic-release/github",
+    ["@semantic-release/git", {
+      "assets": ["README.md", "pyproject.toml", "CHANGELOG.md"],
+      "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+    }],
+  ],
+  preset: "conventionalcommits",
+  releaseRules: [
+    { type: "build", release: "patch" },
+    { type: "chore", release: "patch" },
+    { type: "ci", release: "patch" },
+    { type: "docs", release: "patch" },
+    { type: "feat", release: "minor" },
+    { type: "fix", release: "patch" },
+    { type: "perf", release: "patch" },
+    { type: "refactor", release: "patch" },
+    { type: "revert", release: "patch" },
+    { type: "style", release: "patch" },
+    { type: "test", release: "patch" },
+    { type: "other", release: "patch" },
+  ],
+  parserOpts: {
+    headerPattern: /^(?:[\u{1F300}-\u{1F6FF}\u{2600}-\u{26FF}\u{2700}-\u{27BF}]\s)?(\w*)(?:\((.*)\))?!?:\s(.*)$/u,
+    headerCorrespondence: ['type', 'scope', 'subject'],
+    noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES"],
+  },
+  writerOpts: {
+    commitsSort: ["subject", "scope"],
+  },
 };
