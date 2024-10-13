4d9bbbcf4e831ebc99490a8d28b67dc6a8b3159d
David Hooton <djh00t@users.noreply.github.com>
2024-10-14 10:51:36 +1100

    Update semantic-release.sh

--- .github/workflows/semantic-release.sh
+++ .github/workflows/semantic-release.sh
@@ -1,62 +1,29 @@
 #!/bin/bash
+# Make sure packages are installed
+npm install
 
 # Determine if this is a pre-release or a release
 if [[ "$1" == "pre-release" ]]; then
     RELEASE_TYPE="pre-release"
     RELEASE_BRANCH_PREFIX="rc-v"
-    RELEASERC_FILE=".releaserc-pre-release.js"
-elif [[ "$1" == "release" ]]; then
+    RELEASERC_FILE=$(pwd)/".releaserc.pre-release.js"
+else
     RELEASE_TYPE="release"
     RELEASE_BRANCH_PREFIX="v"
-    RELEASERC_FILE=".releaserc-release.js"
-else
-    echo "Invalid release type. Exiting..."
-    exit 1
+    RELEASERC_FILE=$(pwd)/".releaserc.release.js"
 fi
 
 # Extract the new version from package.json
 NEW_VERSION=$(grep '"version":' package.json | sed 's/.*"version": "\(.*\)",/\1/')
 echo "New version extracted: $NEW_VERSION"
 
-# Function to check if a tag exists
-tag_exists() {
-    git fetch --tags
-    git tag -l | grep -q "^$1$"
-}
-
-# Increment version function
-increment_version() {
-    local version=$1
-    local major=$(echo $version | cut -d. -f1)
-    local minor=$(echo $version | cut -d. -f2)
-    local patch=$(echo $version | cut -d. -f3)
-    patch=$((patch + 1))
-    echo "$major.$minor.$patch"
-}
-
-# Check for version conflicts and increment if necessary
-while tag_exists "v$NEW_VERSION"; do
-    echo "Version $NEW_VERSION already exists. Incrementing version..."
-    NEW_VERSION=$(increment_version $NEW_VERSION)
-    echo "New incremented version: $NEW_VERSION"
-done
-
-# Update the version in package.json
-jq --arg new_version "$NEW_VERSION" '.version = $new_version' package.json > tmp.$$.json && mv tmp.$$.json package.json
-
-# Extract the new description from package.json
-NEW_DESCRIPTION=$(grep '"description":' package.json | sed 's/.*"description": "\(.*\)",/\1/')
-echo "New description extracted: $NEW_DESCRIPTION"
-
-# Update the version and description in pyproject.toml
+# Update the version in pyproject.toml
 if [[ "$OSTYPE" == "darwin"* ]]; then
     echo "Detected macOS"
     sed -i '' -e "s/^version = \".*\"/version = \"$NEW_VERSION\"/" ./pyproject.toml
-    sed -i '' -e "s/^description = \".*\"/description = \"$NEW_DESCRIPTION\"/" ./pyproject.toml
 else
     echo "Detected Linux"
     sed -i'' -e "s/^version = \".*\"/version = \"$NEW_VERSION\"/" ./pyproject.toml
-    sed -i'' -e "s/^description = \".*\"/description = \"$NEW_DESCRIPTION\"/" ./pyproject.toml
 fi
 
 # Update the version in package.json
